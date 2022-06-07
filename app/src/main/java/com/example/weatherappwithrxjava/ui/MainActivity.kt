ee4871894831572a1d80319754fa241bb7050c6e
tejaskhartude <tejaskhartude@gmail.com>
2022-06-07 17:51:21 +0530

    Updates did

--- app/src/main/java/com/example/weatherappwithrxjava/ui/MainActivity.kt
+++ app/src/main/java/com/example/weatherappwithrxjava/ui/MainActivity.kt
@@ -2,11 +2,68 @@ package com.example.weatherappwithrxjava.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Transformation
+import android.widget.Toast
+import androidx.activity.viewModels
+import coil.load
+import coil.transform.RoundedCornersTransformation
 import com.example.weatherappwithrxjava.R
+import com.example.weatherappwithrxjava.databinding.ActivityMainBinding
+import com.example.weatherappwithrxjava.sealed.NetworkResult
+import com.example.weatherappwithrxjava.viewmodel.MainViewmodel
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel by viewModels<MainViewmodel>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fetchData()
+    }
+
+    private fun fetchData() {
+        fetchResponse()
+        mainViewModel.fetchDogresponse()
+        mainViewModel.response.observe(this){ response ->
+            when(response){
+                is NetworkResult.Success ->{
+                    response.data?.let {
+                        binding.imgDog.load(
+                            response.data.message
+                        ){
+                            transformations(RoundedCornersTransformation(16f))
+                        }
+                    }
+                      binding.pbDog.visibility = View.GONE
+                }
+
+                is NetworkResult.Error ->{
+                    //Show the error message
+                    binding.pbDog.visibility = View.GONE
+                    Toast.makeText(this,"Can't fetch, check your connection",Toast.LENGTH_LONG).show()
+                }
+
+                is NetworkResult.Loading ->{
+                    //Loading progress bar
+                    binding.pbDog.visibility = View.VISIBLE
+
+
+                }
+            }
+        }
+    }
+
+    private fun fetchResponse() {
+        mainViewModel.fetchDogresponse()
+        binding.pbDog.visibility = View.VISIBLE
     }
 }
